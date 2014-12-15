dce2b3e0985dc743afda7cffc6472f6034d2b9c9
Talkarcabbage <bobbyrose9@yahoo.com>
2014-12-15 02:08:06 -0600

    First code commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/99/8055189c2e84001416c3fc97dbd10127
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author rrose3
+ * This package contains the majority of transferrable utility classes I write.
+ */
+package com.minepop.talkar.util;
---
+++ RS Timer/src/com/minepop/talkar/timer/Timer.java
@@ -0,0 +1,125 @@
+package com.minepop.talkar.timer;
+
+public class Timer {
+	double startingTime;
+	double duration;
+	String name;
+	int tab;
+	boolean isNormalTimer;
+	
+
+
+	/**
+	 * 
+	 * @param startingTime - The system time the timer started at.
+	 * @param durationTotal - The total time the timer should run for. This is used to reset the timer and check for completion.
+	 * @param name - A name for the timer. This is used as a label in the GUI.
+	 */
+	public Timer(double targetTime, double durationTotal, String name, int tab, boolean isNormalTimer) {
+		this.startingTime = targetTime;
+		this.duration = durationTotal;
+		this.name = name;
+		this.tab = tab;
+		this.isNormalTimer = isNormalTimer;
+	}
+
+	public double getStartingTime() {
+		return startingTime;
+	}
+
+
+	public void setStartingTime(double startingTime) {
+		this.startingTime = startingTime;
+	}
+
+
+	public double getDurationTotal() {
+		return duration;
+	}
+
+
+	public void setDurationTotal(double durationTotal) {
+		this.duration = durationTotal;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public int getTab() {
+		return tab;
+	}
+
+
+	public void setTab(int tab) {
+		this.tab = tab;
+	}
+
+
+	public boolean isNormalTimer() {
+		return isNormalTimer;
+	}
+
+
+	public void setNormalTimer(boolean isNormalTimer) {
+		this.isNormalTimer = isNormalTimer;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(duration);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (isNormalTimer ? 1231 : 1237);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		temp = Double.doubleToLongBits(startingTime);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + tab;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Timer other = (Timer) obj;
+		if (Double.doubleToLongBits(duration) != Double
+				.doubleToLongBits(other.duration))
+			return false;
+		if (isNormalTimer != other.isNormalTimer)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(startingTime) != Double
+				.doubleToLongBits(other.startingTime))
+			return false;
+		if (tab != other.tab)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Timer [startingTime=" + startingTime + ", duration=" + duration
+				+ ", name=" + name + ", tab=" + tab + ", isNormalTimer="
+				+ isNormalTimer + "]";
+	}
+
+	
+}
